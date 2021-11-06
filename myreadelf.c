c1cc7bd16f5a41816d9b0638a53fbed8cef91325
Marcos Paulo de Souza <mpdesouza@suse.com>
2021-11-06 19:14:44 -0300

    Implement section header data reading

--- myreadelf.c
+++ myreadelf.c
@@ -9,13 +9,30 @@
 #include <unistd.h> //close
 
 static int fd;
-static unsigned char elf_header[64];
 static uint64_t ph_off;
 static uint64_t ph_size;
 static uint64_t ph_num;
+
+static uint64_t sh_off;
+static uint64_t sh_size;
+static uint64_t sh_num;
+
+static uint64_t sh_strndx;
+
 static bool is64bit = false;
 
+static unsigned char elf_header[64];
 static unsigned char *prog_header;
+static unsigned char *section_header;
+
+struct sh_entry {
+	int sh_name;
+	int sh_type;
+	int sh_flags;
+	int sh_addr;
+	int sh_offset;
+	int sh_size;
+};
 
 static char *get_prog_type(int type)
 {
@@ -59,6 +76,7 @@ static uint64_t show_var_fields(char *msg, unsigned char *buf, size_t offset,
 				size_t nbytes, bool hex, bool print)
 {
 	unsigned char data[9] = {0};
+	/* Big enough to store 32 and 64 bit values */
 	uint64_t val;
 
 	memcpy(data, buf + offset, nbytes);
@@ -86,9 +104,14 @@ static uint64_t show_prog_field(char *msg, size_t offset, size_t nbytes, bool he
 	return show_var_fields(msg, prog_header, offset, nbytes, hex, true);
 }
 
-static uint64_t get_prog_field(size_t offset, size_t nbytes, bool hex)
+static uint64_t get_prog_field(size_t offset, size_t nbytes)
 {
-	return show_var_fields("", prog_header, offset, nbytes, hex, false);
+	return show_var_fields("", prog_header, offset, nbytes, false, false);
+}
+
+static uint64_t get_section_field(size_t offset, size_t nbytes)
+{
+	return show_var_fields("", section_header, offset, nbytes, false, false);
 }
 
 /* To understand what are the indexes in the elf_header, read the ELF Header format */
@@ -156,12 +179,14 @@ static void show_header_fields()
 	pos = 24;
 	// e_entry
 	show_var_field("Entry point", pos, nbytes, true);
+
 	// e_phoff
 	pos += nbytes;
 	ph_off = show_var_field("Program header offset (bytes)", pos, nbytes, false);
+
 	// e_shoff
 	pos += nbytes;
-	show_var_field("Section header offset (bytes)", pos, nbytes, false);
+	sh_off = show_var_field("Section header offset (bytes)", pos, nbytes, false);
 
 	// e_flags
 	pos += nbytes;
@@ -181,15 +206,15 @@ static void show_header_fields()
 
 	// e_shentsize
 	pos += 2;
-	show_var_field("Size of section headers (bytes)", pos, 2, false);
+	sh_size = show_var_field("Size of section headers (bytes)", pos, 2, false);
 
 	// e_shnum
 	pos += 2;
-	show_var_field("Number of section headers", pos, 2, false);
+	sh_num = show_var_field("Number of section headers", pos, 2, false);
 
 	// e_shstrndx
 	pos += 2;
-	show_var_field("Section header string table index", pos, 2, false);
+	sh_strndx = show_var_field("Section header string table index", pos, 2, false);
 
 	printf("\n");
 }
@@ -198,7 +223,7 @@ static void show_prog_flags(int pos)
 {
 	uint64_t flags;
 	// p_flags
-	flags = get_prog_field(pos, 4, true);
+	flags = get_prog_field(pos, 4);
 	printf("Flags: %c%c%c\n", flags & 0x4 ? 'R' : ' ',
 				flags & 0x2 ? 'W' : ' ',
 				flags & 0x1 ? 'E' : ' ');
@@ -212,7 +237,7 @@ static void show_prog_header()
 
 	/* Type is 4 bytes both in 32 and 64 bit */
 	// p_type
-	type = get_prog_field(pos, 4, false);
+	type = get_prog_field(pos, 4);
 	printf("Type: %s\n", get_prog_type(type));
 	pos += 4;
 
@@ -286,6 +311,88 @@ static void show_program_headers()
 	free(prog_header);
 }
 
+static void show_section_header(struct sh_entry *entry)
+{
+	int nbytes = is64bit ? 8 : 4;
+	int pos;
+
+	entry->sh_name = get_section_field(0, 4);
+	entry->sh_type = get_section_field(4, 4);
+
+	pos = 8;
+	entry->sh_flags = get_section_field(pos, nbytes);
+
+	pos += nbytes;
+	entry->sh_addr = get_section_field(pos, nbytes);
+
+	pos += nbytes;
+	entry->sh_offset = get_section_field(pos, nbytes);
+
+	pos += nbytes;
+	entry->sh_size = get_section_field(pos, nbytes);
+}
+
+static void show_section_headers()
+{
+	struct sh_entry entries[sh_num];
+	uint64_t shstrtab_off;
+	uint64_t shstrtab_size;
+	unsigned char *shstrtab_data;
+	size_t i;
+
+	 /* return if the file does not contain a section header table */
+	if (sh_off == 0)
+		return;
+
+	section_header = malloc(sh_size);
+	if (!section_header)
+		errx(1, "malloc section_header");
+
+	printf("Section Header Table\n");
+	printf("====================\n");
+
+	/* Seek to the start of the section header table */
+	lseek(fd, sh_off, SEEK_SET);
+
+	/*
+	 * Load the values of the section header into entries to print them
+	 * later
+	 */
+	for (i = 0; i < sh_num; i++) {
+		int ret = read(fd, section_header, sh_size);
+		if (ret == -1)
+			errx(1, "section header");
+		show_section_header(&entries[i]);
+
+		/*
+		 * Store the segment pointed by the shstrtab segment headers.
+		 * It will be needed later to get all the section header names.
+		 */
+		if (sh_strndx == i) {
+			shstrtab_off = entries[i].sh_offset;
+			shstrtab_size = entries[i].sh_size;
+		}
+	}
+
+	shstrtab_data = malloc(shstrtab_size);
+	if (!shstrtab_data)
+		errx(1, "malloc shstrtab_data");
+
+	pread(fd, shstrtab_data, shstrtab_size, shstrtab_off);
+
+	/* Print section header data */
+	for (i = 0; i < sh_num; i++) {
+		printf("Section name: %s\n", shstrtab_data + entries[i].sh_name);
+		printf("String name offset: %d\n", entries[i].sh_name);
+		printf("Section offset: %d\n", entries[i].sh_offset);
+		printf("Section Type: %d\n", entries[i].sh_type);
+		printf("\n");
+	}
+
+	free(shstrtab_data);
+	free(section_header);
+}
+
 int main(int argc, char **argv)
 {
 	if (argc != 2)
@@ -297,6 +404,7 @@ int main(int argc, char **argv)
 
 	show_header_fields();
 	show_program_headers();
+	show_section_headers();
 
 	close(fd);
 
