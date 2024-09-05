a53f55999ac9f9b06e6bf419d30b7ac4f86ca756
Marcos Paulo de Souza <mpdesouza@suse.com>
2024-09-05 15:33:24 -0300

    Fix st_value and st_size symbol fields to read 8 bytes

--- myreadelf.c
+++ myreadelf.c
@@ -342,8 +342,8 @@ static void get_symbol(struct sym_tab *t, size_t sym_index, struct sym_entry *en
 		entry->st_info = get_field(&pos, 1);
 		entry->st_other = get_field(&pos, 1);
 		entry->st_shndx = get_field(&pos, 2);
-		entry->st_value = get_field(&pos, 4);
-		entry->st_size = get_field(&pos, 4);
+		entry->st_value = get_field(&pos, 8);
+		entry->st_size = get_field(&pos, 8);
 	} else {
 		entry->st_value = get_field(&pos, 4);
 		entry->st_size = get_field(&pos, 4);
